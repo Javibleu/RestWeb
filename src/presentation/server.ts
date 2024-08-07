e05e7e6b146bb121202492efe012e3a7f5545fcf
Javier <javibleu@hotmail.com>
2024-08-07 21:54:29 +0200

    feat: implement options on server

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,30 +1,41 @@
 import express from 'express';
 import path from 'path';
 
+interface ServerOptions {
+    port: number
+    public_path?: string
+}
 
 export class Server {
 
     private app = express();
- 
-    async start(){
-        
+    private readonly port: number;
+    private readonly publicPath: string;
+
+    constructor(serverOptions: ServerOptions) {
+        this.port = serverOptions.port;
+        this.publicPath = serverOptions.public_path || 'public';
+    }
+
+    async start() {
+
         // Public Folder
-        this.app.use(express.static('public'));
-        
+        this.app.use(express.static(this.publicPath));
+
+
         // Middlewares
         this.app.get('/hello', (req, res) => {
             res.send('Hello World')
         });
-
+        
         this.app.get('*', (req, res) => {
-            const indexPath = path.join(__dirname, '../../public/index.html');
+            const indexPath = path.join(__dirname, `../../${this.publicPath}/index.html`);
             res.sendFile(indexPath);
         })
 
-
-
+        // listen
         this.app.listen(3000, () => {
             console.log('Server is running on http://localhost:3000')
-        })
+        });
     }
 }
