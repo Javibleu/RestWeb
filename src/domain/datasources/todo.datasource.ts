01efabff902ff225c461d03d57e3b26f5102acc3
Javier <javibleu@hotmail.com>
2024-08-13 18:07:57 +0200

    feat: add Repository and Datasource

---
+++ src/domain/datasources/todo.datasource.ts
@@ -0,0 +1,12 @@
+import { CreateTodoDto, UpdateTodoDto } from "../dtos";
+import { TodoEntity } from "../entities/todo";
+
+
+
+export abstract class TodoDataSource {
+    abstract create(createTodoDto: CreateTodoDto): Promise<TodoEntity>;
+    abstract getAll(): Promise<TodoEntity[]>;
+    abstract getById(id: number): Promise<TodoEntity>;
+    abstract update(id: number, updateTodoDto: UpdateTodoDto): Promise<TodoEntity>;
+    abstract delete(id: number): Promise<void>;
+}
