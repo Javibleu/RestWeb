c7bbd967930d20b900ad6cbb0f5ddafd22523626
Javier <javibleu@hotmail.com>
2024-08-13 17:55:59 +0200

    feat: create Entity

---
+++ src/domain/entities/todo.ts
@@ -0,0 +1,14 @@
+
+
+
+class TodoEntity {
+    constructor(
+        public id:number,
+        public title:string,
+        public completedAt?: Date | null
+    ){}
+
+    get isCompleted(){
+        return !!this.completedAt;
+    }
+}
