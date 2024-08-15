a66516a6971d1d27dd34b702da6636304eb26579
Javier <javibleu@hotmail.com>
2024-08-15 19:49:44 +0200

    chore: add barrel file

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -4,3 +4,10 @@ export * from "./dtos";
 export * from "./datasources/todo.datasource";
 export * from "./entities/todo";
 export * from "./repositories/todo.repository";
+
+
+export * from './use-cases/todo/create-todo';
+export * from './use-cases/todo/delete-todo';
+export * from './use-cases/todo/get-todo';
+export * from './use-cases/todo/get-todos';
+export * from './use-cases/todo/update-todo';
